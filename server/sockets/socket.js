fc8fb1c51af9f8df3b35db8d3e56c031f712e038
Fernando Lefloth <mellolefloth@gmail.com>
2023-07-22 19:32:33 -0300

    added feature to send to all users a message

--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -1,6 +1,6 @@
 const { io } = require('../server');
 const {Usuarios} = require('../classes/usuarios')
-
+const { crearMensaje } = require('../utils/utils')
 const usuarios = new Usuarios();
 
 io.on('connection', (client) => {
@@ -24,10 +24,20 @@ io.on('connection', (client) => {
         callback(personas)
     })
 
+    client.on('crearMensaje', (data)=>{
+
+        let persona = usuarios.getPersona(client.id)
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.emit('crearMensaje', mensaje)
+    })
+
+
+
     client.on('disconnect', ()=>{
         let personaBorrada = usuarios.borrarPersona( client.id )
 
-        client.broadcast.emit('crearMensaje', {usuario:'Administrador', mensaje: `${personaBorrada.nombre} abandonó el chat`} )
+        client.broadcast.emit('crearMensaje', crearMensaje('Administrador',`${ personaBorrada.nombre } salió`) )
         client.broadcast.emit('listaPersona', usuarios.getPersonas())
     })
 
